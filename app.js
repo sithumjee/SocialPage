d3720ca5e85f717814a08d71589b0d5b096e17b2
sithum jeevantha <sithum.21@cse.mrt.ac.lk>
2023-12-18 23:56:28 +0530

    NEW FILE

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ let settingsmenu = document.getElementById("updownmenu");
 function settingsMenuToggle() {
   settingsmenu.classList.toggle("settings-menu-height");
 }
+
+function changePage() {
+  window.location.href = "newpage.html";
+}
